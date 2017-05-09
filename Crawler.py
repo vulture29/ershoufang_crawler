f4b255cf6b36de0e5ce247668d7fef9b9e8dc477
xhuang17 <xhuang17@ncsu.edu>
2017-05-09 19:12:54 -0400

    crawl

--- Crawler.py
+++ Crawler.py
@@ -59,20 +59,32 @@ class Crawler:
     def crawlPage(self, pageCode):
         print "Crawl page..."
 
-        # 价格，均价，小区名称，所在区域
-        pattern = re.compile('<div class="overview">.*?<div class=".*?price.*?"><span class="total">(.*?)</span>' + #0
-            '<span class="unit"><span>(.*?)</span>.*?' + #1
-            '<span class="unitPriceValue">(.*?)<i>(.*?)</i>.*?' + #2, 3
-            '小区名称.*?</span>.*?<a.*?>(.*?)</a>.*?' + #4
-            '所在区域.*?</span>.*?<a.*?>(.*?)</a>.*?' #5
-            )
-        items = re.findall(pattern, pageCode)
-        print items
-        if len(items) <= 0:
-            print "CANNOT crawl this page."
-            return items
-        item = items[0]
+        def getReItem(pattern, pageCode):
+            re_pattern = re.compile(pattern)
+            items = re.findall(re_pattern, pageCode)
+            if len(items) <= 0:
+                print "CANNOT crawl this page."
+                quit()
+            return items[0]
+
+        with open('test.htm', 'r') as myfile:
+                pageCode = myfile.read()
+        print "Crawl page..."
+
         ret = []
+        # 标题
+        pattern = '<h1 class="main" title="(.*?)">'
+        item = getReItem(pattern)
+        title = str(item)
+        ret.append(title)
+
+        # 价格，均价，小区名称，所在区域
+        pattern = '<div class="overview">.*?<div class=".*?price.*?"><span class="total">(.*?)</span>' + \
+            '<span class="unit"><span>(.*?)</span>.*?' + \
+            '<span class="unitPriceValue">(.*?)<i>(.*?)</i>.*?' + \
+            '小区名称.*?</span>.*?<a.*?>(.*?)</a>.*?' + \
+            '所在区域.*?</span>.*?<a.*?>(.*?)</a>.*?'
+        item = getReItem(pattern, pageCode)
         price = str(item[0]) + str(item[1])
         avgPrice = str(item[2]) + str(item[3])
         court = str(item[4])
@@ -80,9 +92,61 @@ class Crawler:
         ret.append(price)
         ret.append(avgPrice)
         ret.append(court)
-        ret.append(area)  
-        # 基本属性，房屋标签，经纬度，反馈，小区介绍
-        # To do
+        ret.append(area)
+
+        ## 基本属性
+        # 房屋户型
+        pattern = '<li><span class="label">房屋户型</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 所在楼层
+        pattern = '<li><span class="label">所在楼层</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 建筑面积
+        pattern = '<li><span class="label">建筑面积</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 户型结构
+        pattern = '<li><span class="label">户型结构</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 套内面积
+        pattern = '<li><span class="label">套内面积</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 建筑类型
+        pattern = '<li><span class="label">建筑类型</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 房屋朝向
+        pattern = '<li><span class="label">房屋朝向</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 建筑结构
+        pattern = '<li><span class="label">建筑结构</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 装修情况
+        pattern = '<li><span class="label">装修情况</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 梯户比例
+        pattern = '<li><span class="label">梯户比例</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 供暖方式
+        pattern = '<li><span class="label">供暖方式</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 配备电梯
+        pattern = '<li><span class="label">配备电梯</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
+        # 产权年限
+        pattern = '<li><span class="label">产权年限</span>(.*?)</li>'
+        item = getReItem(pattern, pageCode)
+        ret.append(str(item))
 
         return ret
 
@@ -95,25 +159,34 @@ class Crawler:
 #        socket.socket = socks.socksocket
 #        t = threading.Timer(60, self.reloadTor)
 #        t.start()
-        pageLimit = self.getPageLimit()
-        print('Page limit is ' + str(pageLimit))
-        # Load all pages
-        while(self.pageIndex <= pageLimit):
-        	print "Loading page " + str(self.pageIndex) + "..."
-        	url = 'http://bj.lianjia.com/ershoufang/' + self.crawlArea + '/pg' + str(self.pageIndex)
-        	pageCode = self.getPageCode(url)
-        	self.addLinks(pageCode)
-        	self.pageIndex = self.pageIndex + 1
-        list_file = open(os.getcwd() + '/' + str(self.crawlArea) +'_list.csv', 'w')
-        for item in self.links:
-            list_file.write("%s\n" % item)
+
+        # pageLimit = self.getPageLimit()
+        # print('Page limit is ' + str(pageLimit))
+        # # Load all pages
+        # while(self.pageIndex <= pageLimit):
+        # 	print "Loading page " + str(self.pageIndex) + "..."
+        # 	url = 'http://bj.lianjia.com/ershoufang/' + self.crawlArea + '/pg' + str(self.pageIndex)
+        # 	pageCode = self.getPageCode(url)
+        # 	self.addLinks(pageCode)
+        # 	self.pageIndex = self.pageIndex + 1
+        # list_file = open(os.getcwd() + '/' + str(self.crawlArea) +'_list.csv', 'w')
+        # for item in self.links:
+        #     list_file.write("%s\n" % item)
+
         # Crawling pages
-#         with open(os.getcwd() + '/' + str(self.crawlArea) +'.csv', 'a') as f:
-#             writer = csv.writer(f)
-#             index = ['价格','均价','小区名称','所在区域']
-#             writer.writerow(index)
-#             for link in self.links:
-#                 pageCode = self.getPageCode(link)
-#                 row = self.crawlPage(pageCode)
-#                 writer.writerow(row)
+        with open(os.getcwd() + '/' + str(self.crawlArea) +'.csv', 'a') as f:
+            writer = csv.writer(f)
+            index = ['标题','价格','均价','小区名称','所在区域','房屋户型','所在楼层','建筑面积', '户型结构',\
+            '套内面积','建筑类型','房屋朝向','建筑结构','装修情况','梯户比例','供暖方式','配备电梯','产权年限']
+            writer.writerow(index)
+
+            links_list = []
+            with open(os.getcwd() + '/' + str(self.crawlArea) +'_list.csv', 'rb') as f:
+                reader = csv.reader(f)
+                links_list = list(reader)
+
+            for link in links_list:
+                pageCode = self.getPageCode(link)
+                row = self.crawlPage(pageCode)
+                writer.writerow(row)
 
